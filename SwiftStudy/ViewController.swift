1eca41b4961c249ad947beaa52472a95f1823272
Mr_tang <18242312660@163.com>
2018-05-21 13:17:42 +0800

    View Video Study one

---
+++ SwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by 中发 on 2018/5/21.
+//  Copyright © 2018年 中发. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
