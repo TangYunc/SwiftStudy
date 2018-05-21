669510beec1b348c175f37ce361e2949c652a4ff
Mr_tang <18242312660@163.com>
2018-05-21 14:33:09 +0800

    Xcode8.0 Swift 拓展演练

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -9,63 +9,37 @@
 import UIKit
 
 /**
- 1.OC：[UIView alloc] initWithXXX:]
- Swift:UIView(XXX:)
-       UIView()
-    类名（）== alloc/init 等价
- 
- 2.类方法
- OC: [UIColor redColor]
- Swift:UIColor.red
- 2.0:UIColor.redColor()
- 
- 3.访问当前对象属性，可以不使用:'self'
- 建议：都不用self，在编译器提示的时候再添加，会对‘语境’有更好的体会
- 原因：闭包（类似于OC的block），需要使用self
- 
- 4.没有‘；’
-    ‘；’分割语句的，在 swift中，默认不需要
- 
- 5.枚举类型
- OC：UIButtonTypeContactAdd
- Swift:.contactAdd
- 
- 6.监听方法
- OC:@selector
- Swift:#selector，如果带参数，不需要使用‘：’
- 2.0直接使用“”，带参数需要使用‘：’
- 
- 7.调试
- OC: NSLog
- Swift:print,特点：没有时间，效率比NSLog高
- 
- OC：__Fuction__
- Swift: #function
+ 从Xcode8.0开始，所有插件都不能使用
  */
 
 class ViewController: UIViewController {
 
+    //MARK:--视图生命周期
+    //MARK:视图加载完成
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let a = 10 ; let b = 20
         
+        //MARK:Xcode提供了官方的图片辅助功能Image Literal， 不需要安装第三方插件。 PS：Apple很任性， 很多三方插件都禁用了。
         let v = UIView(frame:CGRect(x: 0, y: 20, width: 100, height: 100));
-        v.backgroundColor = UIColor.red;
+        v.backgroundColor = UIColor.red;//Xcode8.0特有TODO:应该重置新的颜色
+         //ColorLiteral也是Xcode提供的小功能，用法跟ImageLiteral相同。 还可以点击Other按钮，有更多的便捷选色界面。
+        v.backgroundColor = #colorLiteral(red: 0, green: 0.9607843137, blue: 0.9607843137, alpha: 1)
         self.view.addSubview(v)
-        //创建一个按钮
-        let btn = UIButton(type: .contactAdd)
-        //添加到当前视图
-        v.addSubview(btn)
-        btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
-        //2.0语法
-//        btn.addTarget(self, action: "clickMe", for: .touchUpInside)
         
-    }
-    @objc func clickMe(btn: UIButton) -> () {
-        print(#function)
-        print("哈哈哈哈哈")
-        print(btn)
+        //MARK:Image Literal的功能；
+        
+        //1.输入Image会有智能提示， 选中Image Literal并回车。
+        //2.选中ImageLiteral后会显示一个小图标， 双击这个小图标会罗列出Resources目录里的所有图片， 选中你想要的图片就可以了。
+        let im = UIImageView (image: #imageLiteral(resourceName: "头像"))
+        im.frame = CGRect (x: 100, y: 200, width: 300, height: 400)
+        
+        let iv = UIImageView(image: #imageLiteral(resourceName: "背景"))//Xcode8.0特有FIXME:应该更改新的背景
+        
+        iv.center = view.center
+        view.addSubview(iv)
+        view.addSubview(im)
+        
     }
 
     override func didReceiveMemoryWarning() {
