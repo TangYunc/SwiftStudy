b40ef7790e89605334d467e4412c53a7d01f05f2
Mr_tang <18242312660@163.com>
2018-05-23 13:50:07 +0800

    UIButton3.0与4.0不同的地方点

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -17,27 +17,31 @@ import UIKit
  100个swift学习tip，作者王巍，http://onecat.com
  */
 
-class ViewController: UICollectionViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
      
+        /**
+        /// 枚举类型，按钮的nomal在3.0的时候没有，应该是一个bug
+         Swift
+         
+         -- 类
+         -- 结构体
+         -- 枚举（一般不会用到太高级的用法）
+         以上三个都有构造函数，都可以有方法，整体看起来，都像原有的类
+         Xib小，布局快
+        */
+        let btn = UIButton(type: .custom)
+        btn.setTitle("按钮", for: .init(rawValue: 0))
+        btn.setTitle("不是按钮", for: .highlighted)
+        btn.setTitleColor(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), for: .init(rawValue: 0))
+        btn.setTitleColor(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), for: .highlighted)
+        btn.frame = CGRect(x: 10, y: 50, width: 100, height: 40)
+        view.addSubview(btn)
     }
-    //数据源方法
-    //如果使用CollectionViewController的数据源方法都有 ‘override’
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 50
-    }
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        return cell
-    }
-    //移动cell
-    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        
-    }
+    
     
 }
 
