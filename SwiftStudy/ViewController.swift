ccd1ff7639cdb0f8650a4c8ed62922686668d5be
Mr_tang <18242312660@163.com>
2018-05-23 17:23:00 +0800

    有循环引用的代码

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -19,23 +19,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //属性就是一个var
+    var completionCallBack: (() -> ())?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-
-        let l = UILabel()
-        //参数结束{ 尾随闭包 }
-        view.addSubview(l)
-        
-            //Extra argument in call
-            //调用了额外的参数
-        {
-            let l = UILabel()
-            view.addSubview(l)
+        //block 中如果出现self. 要特别小心
+        //只是闭包对self进行了copy，闭包执行完成后，会自动销毁，同时释放对self的引用
+        //要发生循环引用，同时需要self对闭包进行引用
+        loadData {
+            
+            print(self.view)
         }
-        
     }
-    
+    func loadData(completion: @escaping () -> ()) -> () {
+        //使用属性记录闭包    ->self对闭包引用了
+        completionCallBack = completion
+        //异步
+        DispatchQueue.global().async {
+            print("耗时操作")
+            DispatchQueue.main.async(execute: {
+                //回调，执行闭包
+                completion()
+            })
+        }
+    }
+    //类似于OC中的dealloc
+    deinit {
+        print("释放了")
+    }
 }
 
