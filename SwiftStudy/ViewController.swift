e2ad086103e7e9a374e4a7df83ee4191c4db83ad
Mr_tang <18242312660@163.com>
2018-05-23 10:54:38 +0800

    Dictionary

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -22,133 +22,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        demo4()
+     demo3()
     }
-    //MARK:--数组的合并
-    func demo4() -> () {
-        //[String]
-        var arr = ["adsaf","dagq","bqerqrh"]
-//        var arr: [NSObject] = ["adsaf","dagq","bqerqrh"]
-        let arr1 = ["师大会分开"]
-//        let arr1 = ["师大会分开",100]
-        //将arr1的内容合并到arr中去
-        //注意：要合并两个数组，其内容的类型必须一致
-        arr += arr1
-        print(arr)
-        
-    }
-    //MARK:--数组的容量
+    //MARK:
+    //4.合并
     func demo3() -> () {
-        
-        //OC 中，如果初始化容量，可以避免重复分配空间，效率会高一点
-        
-        //定一个数组，存放的是Int类型的数组，但是没有初始化
-//        var arr: [Int]
-//        //给数组进行初始化
-//        arr = [Int]()
-//        print(arr)
-        //以上两句代码合成一句
-        var arr = [Int]()
-        print("初始化容量\(arr.capacity)")
-        for i in 0..<8 {
-            arr.append(i)
-            print("\(arr) 容量\(arr.capacity)")
+        var dict = ["name": "大西瓜", "age": "12", "title": "大哥"]
+        let dict1 = ["name":"张","age":"12"]
+        //思路：遍历dict1，依次设值
+        //如果KEY不存在，会新增
+        //如果KEY存在，会修改
+        for e in dict1 {
+            dict[e.key] = dict1[e.key]
         }
+        print(dict)
     }
-    
-    
-    //MARK:--数组的增。删。改
+    //3.遍历
     func demo2() -> () {
-        //OC中数组分为可变NSMutableArray（var） 和不可变NSArray（let）
-        var arr = ["adsaf","dagq","bqerqrh"]
-        arr.append("oeowow")
-        print(arr)
+        let dict = ["name": "大西瓜", "age": "12", "title": "大哥"]
+        //元组（key:String,value:NSObject）
+        for e in dict {
+            print(e)
+            print("\(e.key)  \(e.value)")
+        }
+        for (key,valu) in dict {
+            print("\(key)   \(valu)")
+        }
         
-        //修改,通过下标定位
-        arr[0] = "qqqq"
-        print(arr)
-        
-        //删除
-        //数组越界Index out of range
-//        arr[5] = "XXXX"
-//        print(arr)
-        arr.remove(at: 3)
-        print(arr)
-        //删除全部，并且保留空间
-        arr.reserveCapacity(4)
-        print("\(arr) \(arr.capacity)")
     }
-    
-    
-    //MARK:--数组的遍历
+    //2.增，删，改
     func demo1() -> () {
-        let arr = ["adsaf","dagq","bqerqrh"]
-        //1.按照下标遍历
-        print("----按照下标遍历")
-        for i in 0..<arr.count {
-            print(arr[i])
-        }
-        //2.for in 遍历元素
-        print("----遍历元素")
-        for s in arr {
-            print(s)
-        }
-        //3.enum block遍历，同时遍历下标和内容
-        print("----遍历下标和内容")
-        for e in arr.enumerated() {
-            print("\(e.offset) \(e.element)")
-        }
-        //4.遍历下标和内容2
-        print("----遍历下标和内容2")
-        /**
-         n是索引下标
-         s是[String]数组 n 对应的String元素
-         */
-        for (n,s) in arr.enumerated() {
-            print("\(n)  \(s)")
-        }
-        //5.反序遍历
-        print("----反序遍历")
-        for s in arr.reversed() {
-            print(s)
-        }
-        
-        //6.反序遍历索引&内容
-        print("----错误的反序遍历")
-        //错误的反序遍历，内容和索引不一致
-        for (n,s) in arr.reversed().enumerated() {
-            print("\(n)  \(s)")
-        }
-        print("----正确的反序遍历")
-        //先枚举，再反序
-        for (n,s) in arr.enumerated().reversed() {
-            print("\(n)  \(s)")
-        }
+        //可变（var）/不可变（let）
+        var dict = ["name":"张","age":"12"]
+        //新增----如果KEY不存在，就是新增
+        dict["title"] = "大哥"
+        print(dict)
+        //修改----如果KEY存在，就是修改
+        dict["name"] = "大西瓜"
+        print(dict)
+        //删除----直接给丁KEY
+        //科普：字典就是通过KEY来定位值 的，KEY必须是可以‘hash哈希’，MD5是哈希的一种
+        //‘哈希’就是将字符串变成唯一的整数，便于查找，提高字典的遍历速度
+        dict.removeValue(forKey: "age")
+        print(dict)
     }
-    
-    
-    //MARK:--数组的定义
+    //1.定义
     func demo() -> () {
-        
-        //OC使用[]定义数组，Swift一样，但是没有‘@’
-        //自动推导结果[String],表示数组中存放的都是 String
-        //跟OC中的数组指定泛型类似
-        let arr = ["adsaf","dagq","bqerqrh"]
+        //OC中定义字典用{}
+        //Swift中定义字典用[]
+         let dict = ["name":"张","age":"12"]
+        let dict1 = ["name":"陆离","age":18] as [String : Any]
+        print(dict)
+        print(dict1)
+        //定义字典数组
+        let arr = [["name":"爱国","age":20],["name":"怪我","age":40],["name":"个哦去","age":1]]
         print(arr)
-        //Swift中基本数据不需要包装
-        let arr1 = [1,2,3,4,5]
-        print(arr1)
-        //CG结构体
-        let point = CGPoint(x: 3, y: 5)
-        let arr2 = [point]
-        print(arr2)
-        //混合结构体，开发中基本不用，因为数组是靠下标索引
-        //如果数组中的类型不一致，自动推导的结果[NSObject]
-        //在Swift中还有一种类型，[AnyObject]->任意对象
-        //在Swift中一个类可以没有父类
-        //****在混合的数组中，CG结构需要包装
-        let arr3 = ["adasf",4,NSValue(cgPoint: point)] as [Any]
-        print(arr3)
+        
     }
 }
 
