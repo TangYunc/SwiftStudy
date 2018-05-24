0f6dd9f2203476910b70a0b17096e72401c764c8
Mr_tang <18242312660@163.com>
2018-05-24 09:25:02 +0800

    Swift推荐的方式解决有循环引用的代码

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -37,8 +37,8 @@ class ViewController: UIViewController {
         //weak 对象可能在运行时被修改 ，->指向的对象一旦被释放，会被自动置为nil
         //ViewController?
 //        weak let weakSelf = self
-        weak var weakSelf = self
-        loadData {
+//        weak var weakSelf = self
+//        loadData {
             //细节2
             //解包有两种方式的解包
             //? 可选解包 - 如果self已经被释放了，不会向对象发出getter消息，更加安全
@@ -48,8 +48,22 @@ class ViewController: UIViewController {
             强行解包，因为需要计算，可选项不能直接参与到计算
         */
             //            print(weakSelf!.view)
-            print(weakSelf?.view)
+//            print(weakSelf?.view)
+//
+//        }
+        
+        //方法二Swift推荐的方法
+        //[weak self]表示{}中所有self都是弱引用，注意解包
+        loadData { [weak self] in
+            print(self?.view)
         }
+        //方法三--Swift的另外一种用法，知道就好
+        //[unowned self]表示{}中所有self都是assign修饰的，不会强引用，但是当对象被释放的时候，指针地址不会变化
+        //如果对象被释放，继续调用，会出现野指针问题
+//        loadData { [unowned self] in
+//            print(self.view)
+//        }
+        
     }
     func loadData(completion: @escaping () -> ()) -> () {
         //使用属性记录闭包    ->self对闭包引用了
