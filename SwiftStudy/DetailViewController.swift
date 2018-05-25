e1ddac8b49115f907228aca8074a9cc09bc44b67
Mr_tang <18242312660@163.com>
2018-05-25 17:06:36 +0800

    通讯录,绑定表格数据&准备明细控制器界面

--- SwiftStudy/DetailViewController.swift
+++ SwiftStudy/DetailViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class DetailViewController: UITableViewController {
 
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBOutlet weak var phoneTF: UITextField!
+    
+    @IBOutlet weak var titleTF: UITextField!
+    
+    var person: Person?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //判断person是否有值，如果有设置UI
+        if person != nil {
+            nameTF.text = person?.name
+            phoneTF.text = person?.phone
+            titleTF.text = person?.title
+        }
+        
     }
 
     
-
+    @IBAction func saveAction(_ sender: Any) {
+    }
+    
 }
