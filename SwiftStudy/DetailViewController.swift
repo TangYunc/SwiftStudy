e2995e6e0ce8c5b973c84e7be6198c08ff7bc243
Mr_tang <18242312660@163.com>
2018-05-25 16:22:55 +0800

    通讯录界面搭建

---
+++ SwiftStudy/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  SwiftStudy
+//
+//  Created by 中发 on 2018/5/25.
+//  Copyright © 2018年 中发. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+
+}
