bbf10662a1a6b9a7dee5a51ed19f9ec7322f5758
Mr_tang <18242312660@163.com>
2018-05-28 15:15:43 +0800

    计算型属性和懒加载的区别

--- SwiftStudy/Person.swift
+++ SwiftStudy/Person.swift
@@ -33,8 +33,17 @@ class Person: NSObject {
         }
     }
     //只读属性的简写,直接写return
+    //又称为计算型属性，本身不保存内容，都是通过计算获得结果
     var title2: String {
+        print("计算name\(name)")
             return "Mr.XXX" + (name ?? "")
     }
+    
+    //懒加载的title本质就是一个闭包
+    //懒加载会在第一次访问的时候，执行。闭包执行结束后，再把执行结果保存在title3中
+    //后续调用直接放回title3 的内容
+    lazy var title3: String? = {
+       return "lazy" + (name ?? "")
+    }()
     
 }
--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+     private lazy var p = Person()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let p = Person()
+        p.title3 = nil
+        
+        //只要调用一次，懒加载后面的闭包就再也不会执行了
+//        let p = Person()
         p.name = "老人"
         print(p.name)
 //        p.title = "afsa"
         print(p.title2)
+        
+        print(p.title3)
+        
+        
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        p.name = "花花"
+        print(p.title2)
+        print(p.title3)
+    }
 }
