e6c3b605be0745ba2a42d632fea427d1d976dbdd
yunchuan <tangyc@zgjzd.cn>
2018-07-31 10:55:32 +0800

    三种try

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -13,26 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let btn = UIButton(type: .custom)
-        btn.frame = CGRect(x: 50, y: 100, width: 100, height: 30)
-        btn.setTitle("Hello World!", for: .init(rawValue: 0))
-        btn.setTitleColor(#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), for: .init(rawValue: 0))
-        btn.sizeToFit()
-        view.addSubview(btn)
+        let str = "{\"name\": \"zhang\"}"
+        guard let data = str.data(using: .utf8) else {
+            return
+        }
+        //1.try? -如果转换失败，大多数会返回nil
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+            return
+        }
+        print(json)
         
-        let btn1 = UIButton(type: .custom)
-        btn1.frame = CGRect(x: 50, y: 150, width: 100, height: 30)
-        btn1.setTitle("Hello World!", for: .normal)
-        btn1.setTitleColor(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), for: .normal)
-        btn1.sizeToFit()
-        view.addSubview(btn1)
+        //2.try! -如果转换失败，直接崩溃，尽量避免使用
+//        let json1 = try! JSONSerialization.jsonObject(with: data, options: [])
+//        print(json1)
+        //3.do try 最安全，但是使用步骤繁琐，另外在其内部智能提示不太好
+        do {
+            let json2 = try JSONSerialization.jsonObject(with: data, options: [])
+            print(json2)
+        } catch {
+            //输出代码的详细错误信息
+            print(error)
+        }
         
-        let btn2 = UIButton(type: .custom)
-        btn2.frame = CGRect(x: 50, y: 200, width: 100, height: 30)
-        btn2.setTitle("Hello World!", for: [])
-        btn2.setTitleColor(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), for: [])
-        btn2.sizeToFit()
-        view.addSubview(btn2)
     }
 
     override func didReceiveMemoryWarning() {
